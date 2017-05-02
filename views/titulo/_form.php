17a8d3fffacd9b571b68f174903eca586bc0b8f9
Vinicius Sarmento <vinicius.eng.comp92@gmail.com>
2017-05-02 19:46:00 -0300

    Alteração de 'Novo' para 'Salvar'

--- views/titulo/_form.php
+++ views/titulo/_form.php
@@ -23,7 +23,7 @@ use app\models\Categoria;
     ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Novo' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
